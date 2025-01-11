6f6f6836501e2f3e1d6e6026a556f894dbb33f04
Thiago Crepequer <thiago.crepequer@hotmail.com>
2025-01-11 16:31:31 -0300

    improvement: adjust next image rule

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const protocol = (process.env.FILE_PROTOCOL || "http") as "http" | "https";
 const hostname = process.env.FILE_HOSTNAME || "localhost";
-const port = process.env.FILE_PORT || "8080";
+const port = process.env.FILE_PORT;
 
 const nextConfig: NextConfig = {
   images: {
