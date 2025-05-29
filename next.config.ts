698507a64b8facc1f770cfdf7e5ceed90e28e10d
thiagocrepequer <thiago.crepequer@hotmail.com>
2025-05-29 20:41:21 -0300

    improvement: change file config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,13 @@
 import "./envConfig.ts";
 import type { NextConfig } from "next";
 
-const protocol = (process.env.FILE_PROTOCOL || "http") as "http" | "https";
-const hostname = process.env.FILE_HOSTNAME || "localhost";
-const port = process.env.FILE_PORT;
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: protocol,
-        hostname: hostname,
-        port: port,
-        pathname: "/file/**",
+        protocol: "https",
+        hostname: "file.my-menu.net",
+        pathname: "/**",
       },
     ],
   },
